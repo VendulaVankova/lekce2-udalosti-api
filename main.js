01178535c542952cfa9330cdddf3b7519406caa6
Vendula Vankova <vendula.van@gmail.com>
2022-02-15 18:51:49 +0100

    using the classList

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 // tady je místo pro náš program
 
 console.log('Ahoj, jsem nalinkován.');
+
+let nadpis = document.querySelector('h1');
+nadpis.classList.add('zluty'); //přidala jsem
+nadpis.classList.remove('zluty'); //odebrala jsem
+
+//zacílený prvek, pomocí classlistu přidaná další třída
+let ctverecek = document.querySelector('div.zluty');
+ctverecek.classList.add('ctverecek');
