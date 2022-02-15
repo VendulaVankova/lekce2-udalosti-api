f474d0b458b92d6fba433c9269517d5f5d62a39f
Vendula Vankova <vendula.van@gmail.com>
2022-02-15 19:06:12 +0100

    Adding headline colour toggle on click

--- main.js
+++ main.js
@@ -15,3 +15,8 @@ function priNajetiNaCtverec() {
     console.log('test ' + pocitadlo);
     pocitadlo++;
 }
+
+function zmenStyl() {
+    nadpis.classList.toggle('zeleny');  //klikem na nadpis se třída přidá nebo odebere
+
+}
