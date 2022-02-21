f9f486ecd2d3388bba926a8edd5b57d3ac1e2d7c
Vendula Vankova <vendula.van@gmail.com>
2022-02-21 16:52:50 +0100

    Audio player buttons added

--- main.js
+++ main.js
@@ -33,6 +33,60 @@ function priOdjetiZTextu() {
 // https://developer.mozilla.org/en-US/docs/Web/API/Event
 
 function stiskKlavesy(udalost) {
-    console.log(udalost);
+    console.log(udalost.key);
 }
 
+
+function startAudio() {
+    let audioFile = document.getElementById('zvukovaStopa');
+    console.log('Pouštím píseň...');
+    audioFile.play();
+}
+
+//posledni commit od Mateje: https://github.com/Matesyi/lekce2-udalosti-api
+
+function pauseAudio() {
+    let audioFile = document.getElementById('zvukovaStopa');
+    console.log('Zastavuji píseň...');
+    audioFile.pause();
+}
+
+/* NOT WORKING :/ 
+function muteAudio() {
+    let audioFile = document.getElementById('zvukovaStopa');
+    console.log('Object.volume'); //1
+    Object.volume = 0.10;
+}
+
+
+function justRightVolume() {
+    let audioFile = document.getElementById('zvukovaStopa');
+    console.log('Object.volume'); //1
+    Object.volume = 0.50;
+}
+
+function maximumVolume() {
+    let audioFile = document.getElementById('zvukovaStopa');
+    console.log('Object.volume'); //1
+    Object.volume = 1;
+} */
+
+// taken from http://www.java2s.com/ref/javascript/javascript-dom-html-audio-volume-property-set.html
+
+let x = document.getElementById("zvukovaStopa");
+
+function getVolume() { 
+  document.getElementById("zvukovaStopa").innerHTML = x.volume;
+} 
+
+function muteAudio() { 
+    x.volume = 0.2;
+  }
+
+function justRightVolume() { 
+  x.volume = 0.5;
+}
+   
+function maximumVolume() { 
+  x.volume = 1.0;
+} 
